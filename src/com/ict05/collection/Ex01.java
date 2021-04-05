6f1dcf7d2cb5bc7839c45376faf11af9e2807f15
eye1228 <dhkajskdlf@naver.com>
2021-04-05 18:15:36 +0900

    0405_Final

--- src/com/ict05/collection/Ex01.java
+++ src/com/ict05/collection/Ex01.java
@@ -3,8 +3,17 @@ package com.ict05.collection;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//제네릭과 컬렉션 (자료구조)
+		//제네릭 : 컬렉션이 어떤 객체들로 이루어졌는지를 표시하는 객체 타입
+		//컬렉션 : 객체들을 모아서 관리하는 인터페이스를 구현한 것.
+		//API : <T> => 객체 타입, <E> => 요소(컬렉션 안에 존재하는 객체 하나를 뜻함)
+		//      Map 형식에서는 <K, V> => Key(키), Value(값)
+		//      Key와 Value가 1:1 대응, Key를 호출하면 Value 가 나옴
+		//형식 : 컬렉션 < 제네릭> 
+		//최상위 컬렉션은 Collection<E>, Map<K, V>
+		//Collection<E>를 상속받은 인터페이스  : Set<E>, List<E>, Queue<E>
+		
+		//Collection의 주요 메소드
 	}
 
 }
